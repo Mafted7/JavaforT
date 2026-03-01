34af7b2d0a29ed79bd96658f1adcfc9030463250
Fadare Oluwatimileyin <timileyinfadare7@gmail.com>
2026-03-01 12:40:53 +0100

    Loops in Java

---
+++ Loops/src/loops/DoWhileQuestionRunner.java
@@ -0,0 +1,20 @@
+package loops;
+
+import java.util.Scanner;
+
+public class DoWhileQuestionRunner {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int number = 0;
+		do {
+			System.out.println("Cube is: " + (number * number * number));
+			System.out.println("Enter a number: ");
+			number = scanner.nextInt();
+		} while (number >= 0);
+		{
+			System.out.println("Thank you have a nice day.");
+		}
+
+	}
+}
